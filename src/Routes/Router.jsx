d824d901c3a4250008f9acf35f5986a9b60b9dca
shihabosm <shihabshamim767@gmail.com>
2024-09-17 18:49:08 +0600

    clear all code

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "./Layout/Main";
-import Home from "./pages/Home/Home/Home";
-import SignIn from "./pages/SignIn/SignIn";
+
+
 import { createBrowserRouter } from "react-router-dom"
 import Main from "../Layout/Main"
 import Home from "../pages/Home/Home/Home"
 import AboutUs from "../pages/AboutUs/AboutUs"
+import SignIn from "../pages/SignIn/SignIn"
+
 
 export const router = createBrowserRouter([
   {
