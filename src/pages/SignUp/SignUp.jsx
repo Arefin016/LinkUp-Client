58c3ca2388870127ad219255f469ca78fc811e19
emon00007 <skemon152@gmail.com>
2024-09-17 15:29:53 +0600

    image add section completed

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import {useRef, useState } from "react";
 import { Helmet } from "react-helmet";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const SignUp = () => {
+  const imageRef =useRef(null)
   const [showPassword, setShowPassword] = useState(false);
+  const [image ,setImage]=useState("")
+
+  const handleImageClick = () => {
+    imageRef.current.click();
+  };
+// const handleImageChange=(e)=>{
+//   const file = e;
+//   setImage('')
+// }
   return (
     <div>
       <Helmet>
@@ -17,8 +27,20 @@ const SignUp = () => {
           <div className="card shrink-0 lg:w-1/3 md:w-2/4 w-2/3 mx-auto shadow-2xl bg-base-100">
             <form className="card-body">
               <div className="form-control">
+              <div onClick={handleImageClick} className="items-center text-center">
+      <label htmlFor="my_modal_8" className="bg-transparent border-none shadow-none">
+        <img
+          className="w-20 h-20 mx-auto border-2 rounded-full border-green-500"
+          src="/public/profile-icon-design-free-vector.jpg"
+          alt="Profile Icon"
+        />
+        <input className="hidden" type="file" ref={imageRef} />
+        <p>Image Select</p>
+      </label>
+    </div>
+
                 <label className="label">
-          
+
                   <span className="label-text">Name</span>
                 </label>
                 <input
@@ -42,16 +64,7 @@ const SignUp = () => {
                 />
               </div>
               <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Photo URL</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Photo URL"
-                  name="photoUrl"
-                  className="input input-bordered"
-                  required
-                />
+
               </div>
               <div className="form-control">
                 <label className="label">
