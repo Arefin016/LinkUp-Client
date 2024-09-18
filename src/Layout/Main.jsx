17e54bdea7573a88cb62a06dfd4e4368e6f70e5f
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-09-18 12:11:18 +0600

    Final Changes Overall

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
-import { Outlet } from "react-router-dom";
-import Footer from "../pages/Shared/Footer/Footer";
-
-import Banner from "../pages/Home/Home/Banner/Banner";
-import Navbar from "../components/navbar/Navbar";
-=======
 import { Outlet } from "react-router-dom"
 import Footer from "../pages/Shared/Footer/Footer"
-import NavBar from "../pages/Shared/NavBar/NavBar"
 
-
->>>>>>> c202035e81acdba4686781e85c0a8953f2fe7e99
+import Banner from "../pages/Home/Home/Banner/Banner"
+import Navbar from "../components/navbar/Navbar"
 
 const Main = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar /> {/* Keeping the Navbar component from hasib-nav */}
       <Banner></Banner>
-=======
-      <NavBar></NavBar>
-      
->>>>>>> c202035e81acdba4686781e85c0a8953f2fe7e99
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
