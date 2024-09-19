602050e62b7baca6da6638fc747aa7488305e52a
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-19 18:02:41 +0530

    navbar small edit

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../pages/Shared/Footer/Footer";
+// import Footer from "../pages/Shared/Footer/Footer";
 
 import Banner from "../pages/Home/Home/Banner/Banner";
 import Navbar from "../components/navbar/Navbar";
-import { Outlet } from "react-router-dom";
+
 import Footer from "../pages/Shared/Footer/Footer";
-import Navbar from "../pages/Shared/Navbar";
+// import Navbar from "../pages/Shared/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar /> {/* Keeping the Navbar component from hasib-nav */}
+      <Navbar /> Keeping the Navbar component from hasib-nav
+      {/* <Navbar /> */}
       <Banner></Banner>
       <Navbar></Navbar>
       <Outlet></Outlet>
