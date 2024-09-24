2649faff6bac9213f28baea4e476897111e55a3a
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-09-24 16:31:26 +0600

    Login and SignUp page has been done but authentication has not been done

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,8 @@
 import { Link, useNavigate } from "react-router-dom"
-import img from "../../../src/assets/login.svg"
 import { useContext } from "react"
 import { AuthContext } from "../../providers/AuthProvider"
+import SignIn from "../../../public/SignIn.json"
+import Lottie from "lottie-react"
 
 const SignUp = () => {
   const { createuser, updateUserProfile } = useContext(AuthContext)
@@ -29,11 +30,13 @@ const SignUp = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
         <div className="w-1/2 mr-12">
-          <img src={img} alt="" />
+          <Lottie animationData={SignIn}></Lottie>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleSignUp} className="card-body">
-            <h1 className="text-3xl text-center font-bold">Sign Up</h1>
+            <h1 className="text-3xl text-center font-bold">
+              Sign Up To Link Up
+            </h1>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -79,9 +82,9 @@ const SignUp = () => {
             </div>
           </form>
           <p className="my-4 text-center">
-            Already Have An Account?{" "}
-            <Link className="text-orange-600 font-bold" to="/login">
-              Login
+            Already registered? Go to{" "}
+            <Link className="text-blue-600 font-bold" to="/login">
+              LOGIN
             </Link>
           </p>
         </div>
@@ -91,144 +94,3 @@ const SignUp = () => {
 }
 
 export default SignUp
-
-// import { useRef, useState } from "react"
-// import { Helmet } from "react-helmet"
-// import { useForm } from "react-hook-form"
-// import { FaRegEye, FaRegEyeSlash } from "react-icons/fa"
-// import { Link } from "react-router-dom"
-// const SignUp = () => {
-//   const imageRef = useRef(null)
-//   const [showPassword, setShowPassword] = useState(false)
-//   const [image, setImage] = useState("")
-//   // React hook form
-//   const {
-//     register,
-//     handleSubmit,
-//     watch,
-//     formState: { errors },
-//   } = useForm()
-
-//   const onSubmit = (data) => {
-//     console.log(data)
-//   }
-
-//   // const handleImageClick = () => {
-//   //   imageRef.current.click()
-//   // }
-
-//   return (
-//     <div className="min-h-screen bg-base-200 mb-10">
-//       <Helmet>
-//         <title>SignUp</title>
-//       </Helmet>
-//       <div className="border-black border rounded-md">
-//         <div className="text-center md:p-3 lg:p-10 my-5 md:text-3xl text-xl font-semibold lg:text-5xl md:font-semibold lg:font-extrabold">
-//           <p>Please Sign Up</p>
-//         </div>
-//         <div className=" flex items-center border-black border rounded-md m-3">
-//           <div className="w-[600px] hidden md:block border-black border rounded-md m-3">
-//             <img
-//               src="/public/pngtree-signing-up-for-a-course-isolated-cartoon-vector-illustrations-picture-image_8710389.png"
-//               alt=""
-//             />
-//           </div>
-//           <form
-//             onSubmit={handleSubmit(onSubmit)}
-//             className="mx-auto border-black border rounded-md m-3"
-//           >
-//             {/* <div> */}
-//             <div className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-//               <form className="card-body">
-//                 <div className="form-control">
-//                   <div
-//                     // onClick={handleImageClick}
-//                     className="items-center text-center"
-//                   >
-//                     <label
-//                       htmlFor="my_modal_8"
-//                       className="bg-transparent border-none shadow-none"
-//                     >
-//                       <img
-//                         className="w-20 h-20 mx-auto border-2 rounded-full border-green-500"
-//                         src="/public/profile-icon-design-free-vector.jpg"
-//                         alt="Profile Icon"
-//                       />
-//                       <input className="hidden" type="file" ref={imageRef} />
-//                       <p>Image Select</p>
-//                     </label>
-//                   </div>
-//                 </div>
-//                 <div className="form-control border-b-2">
-//                   <input
-//                     type="text"
-//                     placeholder="Your Name"
-//                     {...register("name")}
-//                     name="name"
-//                     className="input focus:outline-none"
-//                     required
-//                   />
-//                 </div>
-
-//                 <div className="form-control border-b-2">
-//                   <input
-//                     type="email"
-//                     name="email"
-//                     {...register("email")}
-//                     placeholder="Email"
-//                     className="input focus:outline-none"
-//                     required
-//                   />
-//                 </div>
-//                 <div className="form-control"></div>
-//                 <div className="form-control border-b-2">
-//                   <div className="relative">
-//                     <input
-//                       className="input focus:outline-none w-full py-2 px-4"
-//                       type={showPassword ? "text" : "password"}
-//                       placeholder="Enter Your Password"
-//                       name="password"
-//                       {...register("password")}
-//                       required
-//                     />
-//                     <span
-//                       className="absolute top-4 right-2 cursor-pointer"
-//                       onClick={() => setShowPassword(!showPassword)}
-//                     >
-//                       {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-//                     </span>
-//                   </div>
-//                 </div>
-//                 <div className="form-control mt-6">
-//                   <button className="btn btn-primary">Sign Up</button>
-//                 </div>
-//               </form>
-
-//               <div className="grid grid-cols-3 items-center">
-//                 <span className="ml-3 text-black">
-//                   <hr className="border border-black" />
-//                 </span>
-
-//                 <span className="text-center">OR</span>
-//                 <span className="mr-3 text-black">
-//                   <hr className="border border-black" />
-//                 </span>
-//               </div>
-//               <div className="text-center pb-5">
-//                 <p>
-//                   Already have an account?{" "}
-//                   <Link className="font-bold text-blue-600" to="/login">
-//                     Log In
-//                   </Link>
-//                 </p>
-//               </div>
-//             </div>
-//             {/* </div> */}
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default SignUp
