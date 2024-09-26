0a4c1e1e4ef57854eb06c77fe6c4b2307d84ee3c
Md shaharul Islam <shaharulislam025@gmail.com>
2024-09-26 12:56:13 +0600

    update shihab work banner

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -5,6 +5,7 @@ import SignUp from "../pages/SignUp/SignUp"
 import Home from "../pages/Home/Home/Home"
 import Main from "../Layout/Main"
 import EventHistory from "../pages/Home/EventHistory/EventHistory"
+import MyCalender from "../pages/Home/MyCelender/MyCalendar"
 import Testimonials from "../pages/Testimonials/Testimonials"
 import PrivateRoute from "./PrivateRoute"
 
@@ -33,11 +34,19 @@ export const router = createBrowserRouter([
         path: "/history",
         element: <EventHistory></EventHistory> ,
       },
-      {
+          {
         path: "/testimonials",
         element: (
           <PrivateRoute>
             <Testimonials></Testimonials>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/celender",
+        element: (
+          <PrivateRoute>
+            <MyCalender></MyCalender>
           </PrivateRoute>
         ),
       },
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,11 @@
 import Banner from "../Banner/Banner"
-import MyCalendar from "../MyCelender/MyCalendar"
+
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <MyCalendar></MyCalendar>
+  
     </div>
   )
 }
